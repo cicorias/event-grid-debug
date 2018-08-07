0036cc95202910755d3cf0bbaf3b3fd63f1aed2e
Shawn Cicoria <github@cicoria.com>
2018-08-06 22:28:12 -0400

    updates

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const http = require('http');
+const connect = require('connect');
+const bodyParser = require('body-parser');
+const Convert = require('./models/subscriptionValidationEvent');
 
-http.createServer(function (request, response) {
-    console.log('request ', request.url);
+const app = connect();
 
-    const body = JSON.stringify( { message: 'Event Accepted'} );
-    response.writeHead(200, {
-      'Content-Length': Buffer.byteLength(body),
-      'Content-Type': 'application/json' });
-    response.end(body);
-    console.log('--- end ---')
-    
+// app.use(bodyParser.json());
+app.use(bodyParser.text( { type: 'application/json' } ));
 
-}).listen(8000);
+
+app.use( (request, response) => {
+  console.log('request ', request.url);
+  console.log(request.body);
+  
+  const validationEvent = Convert.toSubscriptionValidation(request.body);
+
+  const responseBody = JSON.stringify( [{ message: 'Event Accepted'}] );
+
+  response
+
+  response.writeHead(200, {
+    'Content-Length': Buffer.byteLength(responseBody),
+    'Content-Type': 'application/json' });
+  response.end(responseBody); 
+  console.log('--- end ---')
+})
+  
+http.createServer(app).listen(8000); 
+
 console.log('Server running at http://127.0.0.1:8000/')
+ 
